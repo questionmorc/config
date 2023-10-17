787b37501759d63314b0b035ff5a1812f0fea687
Marc Alloul <malloul@turbulent.ca>
2023-10-16 22:31:36 -0400

    go ide

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -8,7 +8,7 @@ lspconfig.gopls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   cmd = {"gopls"},
-  filetypes = {"go", "gomod", "gowork", "gotmpl" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
     gopls = {
